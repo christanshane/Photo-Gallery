fbc6147d93a6d38b5c4d3af32eb47eecd3a4f3de
Decebel Geduriagao <iamdecebel@gmail.com>
2015-09-21 18:43:05 +0800

    Photo Gallery Update

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+//Database Constants
+defined('DB_SERVER') ? null : {define("DB_SERVER", "localhost");
+defined('DB_SERVER') ? null : define("DB_USER", "gallery");
+defined('DB_SERVER') ? null : define("DB_PASS", "MySQL2015");
+defined('DB_SERVER') ? null : define("DB_NAME", "photo_gallery");
+
+?>
